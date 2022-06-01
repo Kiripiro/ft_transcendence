4e650987dc6d663fdda1fb9be72b322e68db54fa
Wazack <64anex@gmail.com>
2022-06-01 19:17:48 +0200

    debut de la liste d'amis avec une API REST

--- frontend/src/Page/HomePage/HomePage.tsx
+++ frontend/src/Page/HomePage/HomePage.tsx
@@ -1,40 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import Navbar from '../../Module/Navbar/Navbar';
 import './../assets/Font.css';
 import './HomePage.css'
 
+class HomePage extends Component<{}, {clubs: any[]}> {
 
-const HomePage=() => {
+    constructor(props: any){
+        super(props);
+        this.state = {
+            clubs: []
+        };
+    }
+    
+    componentDidMount(){
+        // fetch('http://localhost:3000/profile')
+        // .then(response => response.json())
+        // .then(profile => console.log(profile));
+        fetch('http://localhost:3000/friends')
+        .then(response => response.json())
+        .then(teams => this.setState({clubs: teams}))
+    }
 
-    return (
-        <div className='Font'>
-            <div className="horizontal">
-                <Navbar/>
-                <div className="vertical">
-                    <main>
-                        <div className="match-history">
-                            <h3>Match History</h3>
+    render(){
+        // var text = document.createTextNode("Tutorix is the best e-learning platform");
+        // tag.appendChild(text);
+        for (let i = 0; i < this.state.clubs.length; i++) {
+            if (this.state.clubs[i].status)
+            {
+                var tag = document.createElement("div");
+                tag.className = "online-friend"
+                var element = document.getElementById("friendsInfo");
+                if (element)
+                    element.appendChild(tag);
+            }
+        }
+        return (
+            <div className='Font'>
+                {/* {this.state.clubs.map(club => (<div className='friends'> {club.user} </div>))}
+                <h1>{this.state.clubs}</h1> */}
+                <div className="horizontal">
+                    <Navbar/>
+                    <div className="vertical">
+                        <main>
+                            <div className="match-history">
+                                <h3>Match History</h3>
+                            </div>
+                            <div className="stat">
+                                <div className="rank"></div>
+                                <div className="graph"></div>
+                            </div>
+                        </main>
+                        <div className="info">
+                            <div className="user-info">
+                                <div className="user-picture"></div>
+                                <p className="username">username</p>
+                                <p className="level">lvl</p>
+                            </div>
+                            <div className="friends-info" id='friendsInfo'>
+                                <h3>Friends</h3>
+                            </div>
+                            <div className="chat"></div>
                         </div>
-                        <div className="stat">
-                            <div className="rank"></div>
-                            <div className="graph"></div>
-                        </div>
-                    </main>
-                    <div className="info">
-                        <div className="user-info">
-                            <div className="user-picture"></div>
-                            <p className="username">Username</p>
-                            <p className="level">Level</p>
-                        </div>
-                        <div className="friends-info">
-                            <h3>Friends</h3>
-                        </div>
-                        <div className="chat"></div>
                     </div>
+                </div>
             </div>
-	</div>
-        </div>
-    );
+        );
+    }
   };
   
   export default HomePage;
