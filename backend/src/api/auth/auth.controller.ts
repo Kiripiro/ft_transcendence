a10c84984d416beacd913f3204407ee535ccd45b
Tourret Alexandre <atourret@student.42lyon.fr>
2022-06-17 16:08:40 +0200

    JsonWebToken implementation in progress

--- backend/src/api/auth/auth.controller.ts
+++ backend/src/api/auth/auth.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Res, Response, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserController } from '../user/user.controller';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
-	@Get('/api42/signin')
-	async signIn42(@Query() query) {
-		return await this.authService.signIn42(query);
+	@Get('/login')
+	@Redirect('http://localhost:3000/HomePage')
+	async login(@Query() query, @Res() response: Response) {
+		const user = await this.authService.login(query);
+		console.log(user);
+		return user;
 	}
+
+	//logout
 }
