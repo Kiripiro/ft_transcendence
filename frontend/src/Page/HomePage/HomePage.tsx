755b1aa257b71dd8d5e9389946083fba278ca9ab
Wazack <64anex@gmail.com>
2022-06-23 15:38:31 +0200

    add: friend add

--- frontend/src/Page/HomePage/HomePage.tsx
+++ frontend/src/Page/HomePage/HomePage.tsx
@@ -1,62 +1,58 @@
 import { rmSync } from 'fs';
 import React, { useState, Component } from 'react';
-import axios from 'axios'
 import Navbar from '../../Module/Navbar/Navbar';
 import './../assets/Font.css';
-import './HomePage.css'
+import './HomePage.css';
+import FriendList from './FriendList';
+import { AddFriendHook, FriendListHook } from './Hooks';
+import AddFriend from './AddFriend';
 
-class HomePage extends Component<{}, {friends: any[]}> {
+const HomePage = (props: any) => {
+    const [isFriendList, setFriendList] = FriendListHook(true);
+    const [isAddFriend, setAddFriend] = AddFriendHook(false);
 
-    updateContent = () => {
-        console.log('test');
-    }
-
-    render(){
-        return (
-            <div className='Font'>
-                <div className="horizontal">
-                    <Navbar/>
-                    <div className="vertical">
-                        <main>
-                            <div className="match-history">
-                                <h3>Match History</h3>
-                                <div className='game game-win'></div>
-                                <div className='game game-win'></div>
-                                <div className='game game-lose'></div>
-                                <div className='game game-lose'></div>
-                                <div className='game game-win'></div>
-                                <div className='game game-lose'></div>
-                                <div className='game game-lose'></div>
-                                <div className='game game-win'></div>
-                                <div className='game game-lose'></div>
-                                <div className='game game-win'></div>
-                                <div className='game game-win'></div>
-                                <div className='game game-lose'></div>
-                            </div>
-                            <div className="stat">
-                                <div className="rank"></div>
-                                <div className="graph"></div>
-                            </div>
-                        </main>
-                        <div className="info">
-                            <div className="user-info">
-                                <div className="user-picture"></div>
-                                <p className="username">username</p>
-                                <p className="level">lvl</p>
-                            </div>
-                            <div className="friends-info" id='friendsInfo'>
-                                <div className="friend-top">
-                                    <h3>Friends</h3>
-                                    <a onClick={this.updateContent}><i className="bi bi-search"></i></a>
-                                </div>
-                            </div>
-                            <div className="chat"></div>
+    return (
+        <div className='Font'>
+            <div className="horizontal">
+                <Navbar/>
+                <div className="vertical">
+                    <main>
+                        <div className="match-history">
+                            <h3>Match History</h3>
+                            <div className='game game-win'></div>
+                            <div className='game game-win'></div>
+                            <div className='game game-lose'></div>
+                            <div className='game game-lose'></div>
+                            <div className='game game-win'></div>
+                            <div className='game game-lose'></div>
+                            <div className='game game-lose'></div>
+                            <div className='game game-win'></div>
+                            <div className='game game-lose'></div>
+                            <div className='game game-win'></div>
+                            <div className='game game-win'></div>
+                            <div className='game game-lose'></div>
                         </div>
+                        <div className="stat">
+                            <div className="rank"></div>
+                            <div className="graph"></div>
+                        </div>
+                    </main>
+                    <div className="info">
+                        <div className="user-info">
+                            <div className="user-picture"></div>
+                            <p className="username">username</p>
+                            <p className="level">lvl</p>
+                        </div>
+                        <div className="friends-info">
+                            {isFriendList && <FriendList />}
+                            {isAddFriend && <AddFriend />}
+                        </div>
+                        <div className="chat"></div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
   };
   
   export default HomePage;
