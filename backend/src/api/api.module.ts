239a44ee137a56cc3e6928bb17fbafbdc2d8d29a
Wazack <64anex@gmail.com>
2022-06-28 10:44:04 +0200

    add FriendListModule to ApiModule

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { FriendListModule } from './friend-list/friend-list.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, FriendListModule, AuthModule],
 })
 export class ApiModule {}
